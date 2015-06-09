e9d1d8470cfcb80e72d59b4bc84c12045ed240ab
samael <samael.65535@gmail.com>
2015-06-09 10:43:53 +0800

    修改目录结构

--- greet/bin/greet.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-var greet = require('../index')
-var argv = require('minimist')(process.argv.slice(2));
-console.log(greet(argv._, argv.drunk))
--- greet/index.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = function (name, drunk) {
-    if(drunk) {
-        return "hello " + name + ", you look sexy today";
-    } else {
-        return "hello, " + name;
-    }
-}
